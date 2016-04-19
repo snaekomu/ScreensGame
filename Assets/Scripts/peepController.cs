d6b58794be51c4654bdc752cc8c11c8c0ef06701
snaekomu <snbjorn@outlook.com>
2016-04-19 18:44:14 +0300

    Added first ability to player
    Desc

--- Assets/Scripts/peepController.cs
+++ Assets/Scripts/peepController.cs
@@ -4,23 +4,41 @@ using System;
 
 public class peepController : MonoBehaviour {
 
-	public GameObject[] artsy;
+	private GameObject[] artsy;
 	private Vector3 destination;
-	private Rigidbody rb;
+	//private Rigidbody rb;
 	private NavMeshAgent agent;
+	private GameObject dest;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody> ();
+		//rb = GetComponent<Rigidbody> ();
 		agent = GetComponent<NavMeshAgent> ();
 		artsy = GameObject.FindGameObjectsWithTag ("Art");
 		int n = UnityEngine.Random.Range (0, artsy.Length);
-		destination = artsy [n].GetComponent<Transform> ().position;
+		if (dest != artsy [n]) {
+			destination = artsy [n].GetComponent<Transform> ().position;
+			dest = artsy [n];
+			agent.SetDestination (destination);
+		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
+	}
+
+	public void ChangeDest () {
+		GameObject d = dest;
+		while (d == dest) {
+			int n = UnityEngine.Random.Range (0, artsy.Length);
+			if (dest != artsy [n]) {
+				destination = artsy [n].GetComponent<Transform> ().position;
+				dest = artsy [n];
+			}
+		}
 		agent.SetDestination (destination);
 	}
+
 }
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -3,18 +3,39 @@ using System.Collections;
 
 public class playerController : MonoBehaviour {
 
-    public float moveSpeed = 10f;
-    private float dt;
+    public float moveSpeed = 10f; //Movement Speed
+	public float radius = 2.5f;
+
+
+    private float dt; //
 	private NavMeshAgent agent;
+	private GameObject[] peeps; //Instanciated peeps
+	private int peepsLayer = 1<<10; //Layer number holding peeps objects
+	private int artLayer = 1<<9; //Layer number holding peeps objects
 
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent> ();
-	}
+		peeps = GameObject.FindGameObjectsWithTag("Peeps");
+	}//Start
 
 	// Update is called once per frame
 	void Update () {
+		//Update movement each frame
 		agent.Move(Vector3.right * moveSpeed * Time.deltaTime * Input.GetAxis ("Horizontal") + Vector3.forward * moveSpeed * Time.deltaTime * Input.GetAxis ("Vertical"));
 
-    }
+		//Check for ability triggers each frame
+		if (Input.GetKeyDown("space")) { firstAbility ();}
+
+    }//Update
+
+	void firstAbility() {
+		//Get targets
+		Collider[] targets = Physics.OverlapSphere (this.transform.position, 2.5f, peepsLayer);
+		Debug.Log (targets.Length);
+		for (int i = 0; i < targets.Length; i++) {
+			//Change destination of targets
+			targets [i].GetComponent<peepController>().ChangeDest();
+		}//for	
+	}//firstAbility
 }
