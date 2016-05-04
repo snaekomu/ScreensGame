23b35caa879de244e8ed3ac2d48f61d117156d87
snaekomu <snbjorn@outlook.com>
2016-05-04 14:37:08 +0300

    New room layout, minor code improvements
    New room layout.
    Peeps won't change destination if yelled before they have reached it.

--- Assets/Scripts/peepController.cs
+++ Assets/Scripts/peepController.cs
@@ -5,22 +5,24 @@ using System;
 public class peepController : MonoBehaviour {
 
 	private GameObject[] artsy;
-	private Vector3 destination;
-	//private Rigidbody rb;
+	private Vector3 destinationVector;
 	private NavMeshAgent agent;
-	private GameObject dest;
+	private GameObject destinationGameObject;
+
+	private bool yellable = false;
 
 	// Use this for initialization
 	void Start () {
 		//rb = GetComponent<Rigidbody> ();
-		agent = GetComponent<NavMeshAgent> ();
-		artsy = GameObject.FindGameObjectsWithTag ("Art");
+		agent = GetComponent<NavMeshAgent> (); //get navigation mesh agent
+		artsy = GameObject.FindGameObjectsWithTag ("Art"); //get all art objects
+		//Give peeps a random destination on start
 		int n = UnityEngine.Random.Range (0, artsy.Length);
-		if (dest != artsy [n]) {
-			destination = artsy [n].GetComponent<Transform> ().position;
-			dest = artsy [n];
-			agent.SetDestination (destination);
-		}
+		if (destinationGameObject != artsy [n]) {
+			destinationVector = artsy [n].GetComponent<Transform> ().position;
+			destinationGameObject = artsy [n];
+			agent.SetDestination (destinationVector);
+		}//if
 
 	}//Start
 	
@@ -30,27 +32,33 @@ public class peepController : MonoBehaviour {
 		if (UnityEngine.Random.Range(0,1000) == 362) {
 			ChangeDest ();
 		}
+
+		if (Vector3.Distance(this.transform.position, destinationVector) < 3) {	//check if it's close to its destination
+			yellable = true;	//if it is allow it to have its destination changed (yellable)
+		} else {
+			yellable = false;	//if it's not set yellable to false to make sure it doesn't change
+		}
 		
 	}//Update
 
 	//25% chance of triggering change destination
 	public void ChangeDestTrigger() {
-		if (UnityEngine.Random.Range(0,3) == 1) {
+		if (yellable && UnityEngine.Random.Range(0,3) == 1) { //check if it's close to the destination (yellable) and 25% chance
 			ChangeDest ();
 		}
 	}//ChangeDestTrigger
 
 	//Change destination
 	public void ChangeDest () {
-		GameObject d = dest;	//Save current destination
-		while (d == dest) {		//Loop through random destinations until we find one that is different to the current one
+		GameObject d = destinationGameObject;		//Save current destination
+		while (d == destinationGameObject) {		//Loop through random destinations until we find one that is different to the current one
 			int n = UnityEngine.Random.Range (0, artsy.Length);		//--Get random destination
-			if (dest != artsy [n]) {
-				destination = artsy [n].GetComponent<Transform> ().position;	//--Check if it's different and set it as the current destination
-				dest = artsy [n];
+			if (destinationGameObject != artsy [n]) {
+				destinationVector = artsy [n].GetComponent<Transform> ().position;	//--Check if it's different and set it as the current destination
+				destinationGameObject = artsy [n];
 			}
 		}
-		agent.SetDestination (destination);		//Set destination to AI
+		agent.SetDestination (destinationVector);		//Set destination to AI
 	}//ChangeDest
 
 }//Class
