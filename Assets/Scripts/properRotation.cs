7163e79268e1c6370c0aaf3532edb9f317eebee3
snaekomu <snbjorn@outlook.com>
2016-05-12 01:26:08 +0300

    Fixed clusterfuck
    All abilities except mop working

---
+++ Assets/Scripts/properRotation.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class properRotation : MonoBehaviour {
+
+	void compensateRotation (int speed){
+		this.transform.Rotate (new Vector3(0, 0, Time.deltaTime * speed));
+	}
+}
